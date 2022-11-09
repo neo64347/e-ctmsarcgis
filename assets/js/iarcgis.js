7c2bfb93acec3476991d99c54646230ffa1f8604
neo64347 <108700280+neo64347@users.noreply.github.com>
2022-11-10 00:34:43 +0800

    Add files via upload

---
+++ assets/js/iarcgis.js
@@ -0,0 +1,26 @@
+require([
+    "esri/config",
+    "esri/Map",
+    "esri/views/SceneView"
+],(esriConfig, Map, SceneView)=> {
+
+
+esriConfig.apiKey = "AAPK8f36d5a7f89743e4be1093808945dfe4xjXNzvzwF2HgTDIBD0Q2XzGJQzOATYQlHw0Kep4cPBOWqRE2ns4ozsG0CKEprMVL";
+    const map = new Map({
+    basemap: "arcgis-topographic",
+    ground: "world-elevation"
+});
+
+const view = new SceneView({
+    map: map,
+    camera: {
+    position: {
+        x: -118.808,
+        y: 33.961,
+        z: 2000 // meters
+    },
+        tilt: 75
+    },
+    container: "viewDiv"
+    });
+});
---
+++ assets/js/messenger.js
@@ -0,0 +1,3 @@
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "103222492509718");
+    chatbox.setAttribute("attribution", "biz_inbox");
